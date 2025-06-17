1fa7b20aa08aec279522556767f5c924eb25208e
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2025-06-16 20:40:13 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
